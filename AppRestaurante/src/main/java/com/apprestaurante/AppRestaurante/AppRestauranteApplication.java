035355cde0512cf8aecdf66244c58e4f9c1edc46
JhojanCR <163024119+JhojanCR@users.noreply.github.com>
2024-07-30 11:07:05 -0500

    Package AppRestaurante

---
+++ AppRestaurante/src/main/java/com/apprestaurante/AppRestaurante/AppRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.apprestaurante.AppRestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRestauranteApplication.class, args);
+	}
+
+}
---
+++ AppRestaurante/src/test/java/com/apprestaurante/AppRestaurante/AppRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apprestaurante.AppRestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
